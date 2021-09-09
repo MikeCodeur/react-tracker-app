ebe272db68df58f2ad30fee9f1ed1568beb3b188
Mike Codeur <cabanas.mickael@gmail.com>
2021-09-09 12:49:38 +0800

    exercises/02-filtrer-tracker

--- src/components/TrackersApp.final.js
+++ src/components/TrackersApp.final.js
@@ -1,15 +1,27 @@
 /* eslint-disable no-unused-vars */
 import * as React from 'react'
+import {FilterTrackers} from './FilterTrackers'
 
 import db from '../data'
 
-
-
 function TrackersApp() {
-  const [allTrackers, setAllTrackers] = React.useState(db);
-  const [filterText, setFilterText] = React.useState("");
-  const [selectedTracker, setSelectedTracker] = React.useState({});
+  const [allTrackers, setAllTrackers] = React.useState(db)
+  const [filterText, setFilterText] = React.useState('')
+  const [selectedTracker, setSelectedTracker] = React.useState({})
 
-  return <div>il y a {allTrackers.length} trackers</div>
+  const handleTextChange = text => {
+    setFilterText(text)
+    const filteredTracker = db.filter(
+      track => track.name.toLowerCase().indexOf(text) === -1,
+    )
+    setAllTrackers(filteredTracker)
+  }
+
+  return (
+    <div>
+      <FilterTrackers onTextChange={handleTextChange} />
+      il y a {allTrackers.length} trackers
+    </div>
+  )
 }
 export {TrackersApp}
