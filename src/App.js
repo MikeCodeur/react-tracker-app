cc32d5f76c4cbad87e071191b96909ee4e1c36eb
Mike Codeur <cabanas.mickael@gmail.com>
2021-11-29 10:37:26 +0800

    reset to cra

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css'
-import {TrackersApp} from './components/TrackersApp'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div className="App-content">
-        <TrackersApp />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
